667c488ff12a34234a949ad5d34b412404c42b74
Roman Svitukha <roman.svitukha@slalom.com>
2019-01-07 14:53:57 -0600

    password hash and salt logic added

---
+++ DotNetCoreApi/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreApi.Data;
+using DotNetCoreApi.DTO;
+using DotNetCoreApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthRepository _repo { get; }
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            //validate request;
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if (await _repo.UserExists(userForRegisterDto.Username)) return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                UserName = userForRegisterDto.Username
+
+            };
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+            return StatusCode(201);//Todo: fix later.
+        }
+    }
+}
